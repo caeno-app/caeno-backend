95897dff625b0df3c9ebd64f7230d1530d048e72
William Van <wvan77@gmail.com>
2020-02-02 19:17:34 -0800

    -Added sort by closest proximity to all elasticsearches
    -Added [serving] as a new column to menu_index

--- elastic_util.py
+++ elastic_util.py
@@ -10,12 +10,23 @@ def elasticMenuQuery(keyword, distance, lat, lon) -> 'json string':
     response = elastic_client.search(
         index="menu_index",
         body={
-            "_source": ["item_name", "brand_id", "calories", "restaurant"],     # omit lat_long which could have 20+ entries
+            "_source": ["item_name", "brand_id", "calories", "restaurant", "serving"],     # omit lat_long which could have 20+ entries
+            "sort": [
+                {
+                    "_geo_distance": {
+                        "lat_lon": [lon, lat],
+                        "order": "asc",
+                        "unit": "mi",
+                        "mode": "min",
+                        "distance_type": "arc"
+                    }
+                }
+            ],
             "query": {
                 "bool" : {
                     "must" : {
                         "multi_match" : {
-                            "fields" : ["restaurant", "item_name"],
+                            "fields" : ["restaurant", "item_name", "serving"],
                             "query" : f"{keyword}"
                         }
                     },
@@ -49,6 +60,17 @@ def elasticRestaurantQuery(keyword, distance, lat, lon) -> 'json string':
     response = elastic_client.search(
         index="restaurant_index",
         body={
+            "sort": [
+                {
+                    "_geo_distance": {
+                        "lat_lon": [lon, lat],
+                        "order": "asc",
+                        "unit": "mi",
+                        "mode": "min",
+                        "distance_type": "arc"
+                    }
+                }
+            ],
             "query": {
                 "bool" : {
                     "must" : {
@@ -77,3 +99,41 @@ def elasticRestaurantQuery(keyword, distance, lat, lon) -> 'json string':
     return json.dumps(listResults)
 
 #elasticRestaurantQuery("taco", 5, 33.6, -117.8)
+
+
+def elasticBrandIDQuery(brand_id, lat, lon) -> 'json string':
+    listResults = list()
+
+    elastic_client = Elasticsearch([GLOBALS.ELASTIC_IP], http_auth=('user1', 'user1'), port=9200, use_ssl=False)
+
+    response = elastic_client.search(
+        index="restaurant_index",
+        body={
+            "sort":[
+                {
+                    "_geo_distance" : {
+                        "lat_lon" : [lon, lat],
+                        "order" : "asc",
+                        "unit" : "mi",
+                        "mode" : "min",
+                        "distance_type" : "arc"
+                    }
+                }
+            ],
+            "query": {
+                "bool" : {
+                    "must" : {
+                        "term" : { "brand_id" : brand_id }
+                      }
+                }
+            }
+        }
+    )
+
+    for doc in response['hits']['hits']:
+        listResults.append(doc["_source"])
+
+    return json.dumps(listResults)
+
+
+#print(elasticBrandIDQuery("513fbc1283aa2dc80c000020", 33.6, -117.8))
